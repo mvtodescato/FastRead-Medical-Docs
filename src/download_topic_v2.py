43d91c2a4ea3cde397ff78a247bce49907d9edb9
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2020-07-02 11:15:27 -0300

    Fix download

--- src/download_topic_v2.py
+++ src/download_topic_v2.py
@@ -17,7 +17,7 @@ for topic in arquivos:
     for line in arq_topic:
         line = line.rstrip('\n')
         list_aux = line.split(' ')
-        line = list_aux[1]
+        line = list_aux[7]
         if line not in list_topics:
             list_topics.append(line)
     arq_topic.close()
@@ -52,10 +52,8 @@ arq_content.close()
 
 linha = ['Document Title','Abstract','Year','PDF Link','label']
 for j in range(len(list_topicN)):
-    if list_topicN[j] in ["CD007394", "CD008691", "CD010632"]
-    #if list_topicN[j] == 'CD007394' or list_topicN[j] == 'CD007427' or list_topicN[j] == 'CD008054' or list_topicN[j] == 'CD008643' or list_topicN[j] == 'CD008686' or list_topicN[j] == 'CD008691' or list_topicN[j] == 'CD009020' or list_topicN[j] == 'CD009323' or list_topicN[j] == 'CD009593' or list_topicN[j] == 'CD009944' or list_topicN[j] == 'CD010409' or list_topicN[j] == 'CD010438' or list_topicN[j] == 'CD010632' or list_topicN[j] == 'CD010771asd' or list_topicN[j] == 'CD011134' or list_topicN[j] == 'CD011548' or list_topicN[j] == 'CD011549' or list_topicN[j] == 'CD011975' or list_topicN[j] == 'CD011984':
-        #continue
-
+    if list_topicN[j] == 'CD010438':
+        continue
     nomeCSV = '../workspace/data/' + list_topicN[j] + '.csv'
     with open(nomeCSV, 'w') as file:
         writer = csv.writer(file)
@@ -102,4 +100,4 @@ for j in range(len(list_topicN)):
                         writer.writerow(row)
                         row = ['','','','','0']
                         wtr = 1
-    file.close()
+        file.close()
